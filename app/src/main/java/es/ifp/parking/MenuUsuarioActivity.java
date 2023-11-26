2d53ec5223d0b06875a46d91e80214eaaa1663b9
EfraDev <efraincasarello@gmail.com>
2023-11-26 21:10:42 +0100

    1

--- app/src/main/java/es/ifp/parking/MenuUsuarioActivity.java
+++ app/src/main/java/es/ifp/parking/MenuUsuarioActivity.java
@@ -42,6 +42,9 @@ public class MenuUsuarioActivity extends AppCompatActivity {
         botonBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent pasarPantalla= new Intent(MenuUsuarioActivity.this, BuscarActivity.class);
+                finish();
+                startActivity(pasarPantalla);
 
             }
         });
